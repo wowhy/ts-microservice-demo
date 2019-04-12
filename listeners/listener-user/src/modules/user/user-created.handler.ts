29f3dce5b7700f5b533278fa70c227f4df15e01a
wowhy <wowhy@outlook.com>
2019-04-12 17:50:20 +0800

    调整代码

--- listeners/listener-user/src/modules/user/user-created.handler.ts
+++ listeners/listener-user/src/modules/user/user-created.handler.ts
@@ -5,10 +5,6 @@ export class UserCreatedHandler implements IEventHandler {
   static topic = 'user.create'
 
   async handle(data: any) {
-    if (Math.random() * 10 > 7) {
-      
-    } else {
-      throw new Error('random exception')
-    }
+    console.log(data)
   }
 }
